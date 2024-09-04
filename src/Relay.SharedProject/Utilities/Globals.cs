549c53c5830802af69e9e1b87fa858b9dec4f413
Charlie Jones <charliejones@hoarelea.com>
2024-09-04 13:10:10 +0100

    Revert "source"
    This reverts commit 6d3281b3aadb8f8948ff383bf4a3b0e2870b4783.

--- src/Relay.SharedProject/Utilities/Globals.cs
+++ src/Relay.SharedProject/Utilities/Globals.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using static Relay.Utilities.RibbonUtils;
 
 namespace Relay.Utilities
 {
@@ -15,10 +18,25 @@ namespace Relay.Utilities
         public static Assembly ExecutingAssembly = Assembly.GetExecutingAssembly();
         public static string[] EmbeddedLibraries = ExecutingAssembly.GetManifestResourceNames().Where(x => x.EndsWith(".dll")).ToArray();
         public static string[] PotentialTabDirectories { get; set; }
-        public static string RibbonTabName = "Relay";
+		public static string Discipline { get; set; }
+		public static string RibbonTabName = "Hoare Lea";
         public static string RelayGraphs = Path.Combine(Globals.ExecutingPath, RibbonTabName);
+		public static string GraphDetailPath = Path.Combine(Globals.ExecutingPath, RibbonTabName,"file_settings.json");
+		public static IList<DynamoGraphFileInfo> GraphFileInfo { get; set; }
 
-        public static string CurrentGraphToRun { get; set; } = "";
+		public static string CurrentGraphToRun { get; set; } = "";
 
-    }
+		public class DynamoGraphFileInfo
+		{
+			[JsonProperty("fileName")]
+			public string fileName { get; set; }
+			[JsonProperty("dynamoName")]
+			public string dynamoName { get; set; }
+			[JsonProperty("tooltip")]
+			public string tooltip { get; set; }
+			[JsonProperty("discipline")]
+			public string discipline { get; set; }
+		}
+
+	}
 }
